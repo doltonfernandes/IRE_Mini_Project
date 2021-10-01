3dc396891d797fa71596f1f11014ad38f9a9c89e
Dolton Fernandes <dolton.fernandes@gmail.com>
2021-10-01 17:42:24 +0530

    Optimized to give results within 6s

--- query.py
+++ query.py
@@ -1,9 +1,9 @@
 import os
 import re
 import sys
-import json
 import time
 import math
+import pickle
 import linecache as lc
 from nltk.stem import PorterStemmer
 from collections import defaultdict
@@ -11,6 +11,9 @@ from collections import defaultdict
 start = time.time()
 invertedIdxPath = sys.argv[1]
 query = sys.argv[2]
+
+with open(invertedIdxPath + "/docFreq.txt", "rb") as fp:
+	arr = pickle.load(fp)
 
 emptyQuery = {
 	"title": [],
@@ -47,13 +50,21 @@ invertedIdxFiles = [ f for f in os.listdir(invertedIdxPath) if os.path.isfile(os
 # Files holding titles
 titleFiles = [ f for f in os.listdir(invertedIdxPath) if os.path.isfile(os.path.join(invertedIdxPath, f)) and f.count('_') == 2]
 
-# Returns inverted index filname having posting list for word 'w'
+# Returns inverted index filname having posting
+# list for word 'w' using binary search
 def getInvIdxFileName(w):
-	for f in invertedIdxFiles:
-		lowerW = f.split('.')[0].split('_')[0]
-		upperW = f.split('.')[0].split('_')[1]
-		if w >= lowerW and w <= upperW:
-			return f
+	i = 0
+	j = len(invertedIdxFiles) - 1
+	while i <= j:
+		m = i + ((j - i) // 2)
+		lowerW = invertedIdxFiles[m].split('_')[0]
+		upperW = invertedIdxFiles[m].split('_')[1].split('.')[0]
+		if lowerW <= w and w <= upperW:
+			return invertedIdxFiles[m]
+		if w < lowerW:
+			j = m - 1
+		else:
+			i = m + 1
 	return ""
 
 # Returns inverted index for word 'w'
@@ -65,7 +76,7 @@ def getInvertedIndex(w):
 	match = []
 	with open(invertedIdxPath + '/' + fileName, 'r') as f:
 		for line in f:
-			if line.split('-')[0] == w:
+			if line.startswith(w + '-'):
 				match = line.strip().split('-')[1:]
 				break
 	return match
@@ -128,14 +139,14 @@ docScores = defaultdict(float)
 
 # Returns number of words in doc with doc ID = 'docId'
 def getNumWords(docId):
-	return int(lc.getline(invertedIdxPath + '/docFreq.txt', int(docId) + 1))
+	return arr[int(docId)]
 
 # adds score for word 'word' in field 'field'
 def addScore(field, postingList):
 	if len(postingList) == 0:
 		return
 
-	tf = (1 + math.log(21384756 / len(postingList)))
+	tf = (1 + math.log(21384756 / (1 + len(postingList))))
 	for p in postingList:
 		splittedS = p.split('-')
 		docScores[splittedS[0]] += weights[field] * tf * math.sqrt(int(splittedS[1]) / getNumWords(splittedS[0]))
@@ -144,34 +155,33 @@ for w in ans.keys():
 	for f in ans[w].keys():
 		addScore(f, ans[w][f])
 
-docsWithScores = [[k, v] for k, v in docScores.items()]
-docsWithScores = sorted(docsWithScores, key=lambda x: x[1], reverse=True)
-
 # get top 10 docs
-docsWithScores = docsWithScores[:10]
-
-end = time.time()
+docsWithScores = [[k, v] for k, v in docScores.items()]
+docsWithScores = sorted(docsWithScores, key=lambda x: x[1], reverse=True)[:10]
 
 # Returns title filname having title for doc with doc ID = 'docId'
 def getTitleFileName(docId):
 	docId = int(docId)
-	for f in titleFiles:
-		lowerID = int(f.split('.')[0].split('_')[1])
-		upperID = int(f.split('.')[0].split('_')[2])
-		if docId >= lowerID and docId <= upperID:
-			return f
+	i = 0
+	j = len(titleFiles) - 1
+	while i <= j:
+		m = i + ((j - i) // 2)
+		lowerID = int(titleFiles[m].split('.')[0].split('_')[1])
+		upperID = int(titleFiles[m].split('.')[0].split('_')[2])
+		if lowerID <= docId and docId <= upperID:
+			return titleFiles[m]
+		if docId < lowerID:
+			j = m - 1
+		else:
+			i = m + 1
 	return ""
 
 # returns title for doc with doc ID = 'docId'
 def getTitle(docId):
 	fileName = getTitleFileName(docId)
-	f = open(invertedIdxPath + '/' + fileName)
-	for line in f:
-		if line.startswith(docId + '-'):
-			f.close()
-			return '-'.join(line.strip().split('-')[1:])
-	f.close()
-	return ""
+	return '-'.join(lc.getline(invertedIdxPath + '/' + fileName, int(docId) - int(fileName.split('_')[1]) + 1).split('-')[1:])
+
+end = time.time()
 
 for i in range(len(docsWithScores)):
 	docsWithScores[i][1] = getTitle(docsWithScores[i][0])
